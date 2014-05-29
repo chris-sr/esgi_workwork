82cf2dc2b8269ba8ad82d47229281ce58996e336
Chris SR <csinronia@gmail.com>
2014-05-30 00:29:44 +0200

    CHSR : Test commit branche IHM

---
+++ IHM/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "application/application.h"
+#include "factory/factory.h"
+#include "factory/chain.h"
+#include "factory/worker.h"
+#include "factory/tools.h"
+#include "factory/types.h"
+#include "factory/resource.h"
+#include <vector>
+
+int main(){
+    /*ww::Worker* wa = new ww::Worker();
+    ww::Worker* wb = new ww::Worker();
+    //ww::Factory::destroy();
+    ww::Worker* wc = new ww::Worker();
+    ww::Chain* ca = new ww::Chain();
+    ww::Chain* cb = new ww::Chain();
+    ww::Resource ra;
+    ww::Resource rb;
+    ww::Resource rc;
+    ca->affect_worker(wa);
+    ca->affect_worker(wb);
+    ca->affect_worker(wc);
+    cb->affect_worker(wa);
+    cb->affect_worker(wb);
+    cb->affect_worker(wc);
+
+    ww::Factory::get_reference().print();
+    ww::Factory::destroy();*/
+
+    /*Application app;
+    app.run();*/
+
+    //myfunction = [](int a) -> bool { if a > 10 return true; return false; };
+    //myfunction = [](int x, int y) -> int { return x + y; };
+
+    return 0;
+}
+
